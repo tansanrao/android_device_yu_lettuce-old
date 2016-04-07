5b29db28a0dc21ed5cf69ad9d316c29f75edc7d6
Bharat Pawar <bpawar@codeaurora.org>
2016-04-07 21:33:40 +0530

    QCamera2: Disable CLANG
    Disabled CLANG.
    
    Change-Id: I7c807cfc8bb3aeeb3ade573177139c5f29027a13

--- camera/QCamera2/stack/mm-camera-test/Android.mk
+++ camera/QCamera2/stack/mm-camera-test/Android.mk
@@ -103,6 +103,7 @@ LOCAL_SHARED_LIBRARIES:= \
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE:= mm-qcamera-app
+LOCAL_CLANG := false
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_EXECUTABLE)
 endif
@@ -209,6 +210,7 @@ LOCAL_SHARED_LIBRARIES:= \
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE:= libmm-qcamera
+LOCAL_CLANG := false
 LOCAL_32_BIT_ONLY := true
 include $(BUILD_SHARED_LIBRARY)
 endif
