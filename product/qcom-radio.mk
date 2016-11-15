5dae44d608011ceae335a230530de13bf2d8eb89
Pranav <reddy.pranav89@gmail.com>
2016-11-15 13:18:43 +0000

    lettuce: Update Radio props

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -6,7 +6,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ro.config.always_show_roaming=true \
-    ro.telephony.default_network=9,9
+    ro.telephony.default_network=9,9 \
+    persist.radio.data_no_toggle=1 \
+    persist.data.mode=concurrent \
+    mdc_initial_max_retry=10 \
+    persist.radio.custom_ecc=1 \
+    persist.radio.sw_mbn_update=1
 
 # RIL
 ifeq ($(QCPATH),)
